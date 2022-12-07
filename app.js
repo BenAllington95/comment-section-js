e018b7e9054f7ea9d81f64c5e03f0522356bf474
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2022-12-07 18:56:51 +0000

    Adding functions to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById("comment-btn").addEventListener("click", function(e) {
+    e.preventDefault()
+
+    const formName = document.getElementById("name-el").value
+    const formComment = document.getElementById("comment-el").value
+
+    comments.unshift(
+        {
+
+        })
+       
+
+})
+
+const comments = []
+
