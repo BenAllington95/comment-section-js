9fcabe2b5d96b70199ccd82a74d6a3270c82abb6
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2022-12-07 19:29:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-
+const comments = []
+const d = new Date()
 
 document.getElementById("comment-btn").addEventListener("click", function(e) {
     e.preventDefault()
+
     comments.unshift(
         {
             name: document.getElementById("name-el").value,
-            comment: document.getElementById("comment-el").value
-        })
-    
-    console.log(comments)
-       
+            comment: document.getElementById("comment-el").value,
+            date: d.getDate()
+        })   
+    document.getElementById("name-el").value = ""
+    document.getElementById("comment-el").value = ""
+    render()
 })
 
-const comments = []
 
+const generateHtmlComments = () => {
+    const htmlString = comments.map(comment =>
+         `<div class="comment">
+            <h3>${comment.name}</h3>
+            <p>"${comment.date}"</p>
+            <p>"${comment.comment}"</p>
+        </div>`
+    ).join('')
+
+    return htmlString
+}
+
+const render = () => {
+    document.getElementById("feed").innerHTML = generateHtmlComments()
+}
